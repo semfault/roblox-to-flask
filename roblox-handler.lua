f8ffd5829c80d50d27a954dba19d754c202fb8d1
OpenSauceRepositories <106570163+OpenSauceRepositories@users.noreply.github.com>
2022-11-05 21:32:20 +0800

    Update roblox-handler.lua

--- roblox-handler.lua
+++ roblox-handler.lua
@@ -1,8 +1,9 @@
 local httpserv = game:GetService("HttpService")
 local networksettings = {
-    url = "127.0.0.1",
-    port = "8080"
-  }
-local baseurl = "https://"..networksettings[url]..":"..networksettings[port].."/"
+	["ip"] = "127.0.0.1",
+	["port"] = "8080"
+}
+local baseurl = "https://"..networksettings["ip"]..":"..networksettings["port"]
 
-httpserv:GetAsync(baseurl.."/get")
+httpserv:PostAsync(baseurl.."/post", "test 123", Enum.HttpContentType.ApplicationUrlEncoded, false)
+httpserv:GetAsync(baseurl.."/get", false)
